a992ce4b16d0f6c3c16968e97764c21b09942b13
grust101 <grust101@github.com>
2018-10-03 15:31:38 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# key_phrase_commenter_and_assigner
+
+> A GitHub App built with [Probot](https://probot.github.io) that Will auto comment when someone says &quot;I want to help&quot; and other key phrases with welcome info and will assign that user to the issue
+
+## Setup
+
+```sh
+# Install dependencies
+npm install
+
+# Run the bot
+npm start
+```
+
+## Contributing
+
+If you have suggestions for how key_phrase_commenter_and_assigner could be improved, or want to report a bug, open an issue! We'd love all and any contributions.
+
+For more, check out the [Contributing Guide](CONTRIBUTING.md).
+
+## License
+
+[ISC](LICENSE) © 2018 Georgia Rust <grust101@github.com>
---
+++ index.js
@@ -0,0 +1,31 @@
+/**
+ * This is the entry point for your Probot App.
+ * @param {import('probot').Application} app - Probot's Application class.
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+
+  app.on('issues.opened', async context => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
+
+module.exports = app => {
+  app.on('issues.opened', async context => {
+    // `context` extracts information from the event, which can be passed to
+    // GitHub API calls. This will return:
+    //   {owner: 'yourname', repo: 'yourrepo', number: 123, body: 'Hello World!}
+    const params = context.issue({body: 'Hello World!'})
+
+    // Post a comment on the issue
+    return context.github.issues.createComment(params)
+  })
+}
